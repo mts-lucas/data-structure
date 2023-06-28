95cea9c76b6181db90c83a7d91de56f14618c88a
Lucas Mateus <lmateus1067@outlook.com>
2023-06-28 15:07:07 -0300

    melhor caso

--- atividade_u02/fontes/hash_table.py
+++ atividade_u02/fontes/hash_table.py
@@ -7,19 +7,21 @@ import vetores
 
 class HashTable:
     def __init__(self):
+
+        # caso medio e pior caso
         self.size = 1
         self.table = [[]]
+
+        # melhor caso
+        # self.size = 2
+        # self.table = [[], [25]]
 
     def _hash_function(self, key):
         return key % self.size
 
-        # pior caso
+        # pior caso e melhor caso
 
         # return 0
-
-        # melhor caso
-
-        # return key
 
     def _re_hash(self):
         self.size = self.size * 2
@@ -34,13 +36,17 @@ class HashTable:
                     self.insert(item[0])
 
     def insert(self, value):
+
         if len(self.table[self._hash_function(value)]) >= self.size:
             self._re_hash()
 
         self.table[self._hash_function(value)].append(value)
 
     def search(self, value):
+        # pior e caso medio
         index = self._hash_function(value)
+        # melhor caso
+        # index = 1
         if self.table[index]:
             for item in self.table[index]:
                 if item == value:
@@ -62,8 +68,10 @@ if __name__ == "__main__":
     start = time.time_ns()
     # caso medio
     hasht.search(random.randint(1, 100000))
-    # melhor e pior caso
+    # pior caso
     # hasht.search(n)
+    # melhor caso
+    # hasht.search(25)
     end = time.time_ns()
     final_time = end - start
     print(final_time)
